f89cfc5b5b7e480f268d1e0b2259f3f5d2ae25ef
TopologicalQuantumRose <spacecurvature@gmail.com>
2018-09-03 19:53:04 -0700

    second upload

---
+++ script/contactform.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$title = $_POST['title'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+
+	$mailTo = "huntf@sfu.ca";
+	$headers = "From: ".$email;
+	$from = "Email: ".$email;
+	$txt = "Message from: ".$name".\n\n".$message;
+	
+	mail($mailTo,$title,$txt,$headers);
+	header("Location: index.php?mailsent");
+}
